8b12fe7c2f3c109abfa1a1615d2d75f04288196a
Gustavo Duarte E Silva <63934204+gustavo-duarte-silva@users.noreply.github.com>
2022-10-04 09:08:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 
-st.stop() -- deixa de executar daqui pra baixo
+st.stop() ## deixa de executar daqui pra baixo
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
